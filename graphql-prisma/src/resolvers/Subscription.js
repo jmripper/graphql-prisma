837403fe0c8f96a1cd4eb43515b21e0662b0bd07
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-04-13 15:01:16 -0400

    jwt token process created

--- graphql-prisma/src/resolvers/Subscription.js
+++ graphql-prisma/src/resolvers/Subscription.js
@@ -1,34 +1,28 @@
 const Subscription = {
   comment: {
-    subscribe(parent, { postId }, { prisma }, info) {
-      return prisma.subscription.comment(
-        {
-          where: {
-            node: {
-              post: {
-                id: postId,
-              },
-            },
-          },
-        },
-        info
-      );
-    },
+      subscribe(parent, { postId }, { prisma }, info){
+          return prisma.subscription.comment({
+              where: {
+                  node: {
+                      post: {
+                          id: postId
+                      }
+                  }
+              }
+          }, info)
+      }
   },
   post: {
-    subscribe(parent, args, { prisma }, info) {
-      return prisma.subscription.post(
-        {
-          where: {
-            node: {
-              published: true,
-            },
-          },
-        },
-        info
-      );
-    },
-  },
-};
+      subscribe(parent, args, { prisma }, info) {
+          return prisma.subscription.post({
+              where: {
+                  node: {
+                      published: true
+                  }
+              }
+          }, info)
+      }
+  }
+}
 
-export { Subscription as default };
+export { Subscription as default }
